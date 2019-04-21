ec07526d88e54045e275bfbebf24ea0c5fcd288c
Grant Kazan <grantkazan@gmail.com>
2019-04-21 14:14:33 -0500

    display showtimes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Showtimes Available</h1>
+    <div v-for="showtime in showtimes" class="showtime">
+      <p>Movie: {{ showtime.title }}</p>
+      <p>Theater: {{ showtime.theater_id }}</p>
+      <p>Time: {{ showtime.time }}</p>
+      <p>
+        Soldout?
+        {{
+          showtime.status.is_sold_out
+            | moment("timezone", "America/Los_Angeles", "LLLL ss")
+        }}
+      </p>
+    </div>
   </div>
 </template>
 
+<style>
+.showtime {
+  border: solid gray;
+  margin-bottom: 1.5rem;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require("axios");
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to My Site!",
+      showtimes: []
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/showtimes").then(
+      function(response) {
+        console.log(response);
+        this.showtimes = response.data;
+      }.bind(this)
+    );
+  },
+  methods: {},
+  computed: {}
+};
 </script>
