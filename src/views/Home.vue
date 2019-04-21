b71577d9b31519c52fa7d9e43692797471d0eaff
Grant Kazan <grantkazan@gmail.com>
2019-04-21 17:56:39 -0500

    added buy tickets functionality with validations and error messages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
     <h1>Showtimes Available</h1>
-    <div v-for="showtime in showtimes" class="showtime">
+    <ul class="error">
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+    <div
+      v-for="showtime in showtimes"
+      v-bind:showtime="showtime"
+      class="showtime"
+    >
       <p>Movie: {{ showtime.title }}</p>
       <p>Theater: {{ showtime.theater_id }}</p>
       <p>Time: {{ showtime.time }}</p>
@@ -11,6 +18,23 @@
       </p>
       <div v-if="showtime.status.is_sold_out == false">
         <button v-on:click="buyTicket(showtime)">BUY!</button>
+        <p>Buy Ticket</p>
+        <div>
+          <p>name: <input type="text" v-model="name" /></p>
+          <p>email: <input type="text" v-model="email" /></p>
+          <p>
+            credit_card: <input type="number" v-model.number="credit_card" />
+          </p>
+          <p>
+            expiration_date: <input type="date" v-model="expiration_date" />
+          </p>
+          <p>{{ showtime.id }}</p>
+        </div>
+        <!--         name
+        email
+        credit_card
+        expiration_date
+        showtime_id -->
       </div>
       <div v-else class="sold_out">
         <p>SOLD OUT</p>
@@ -25,8 +49,13 @@
   margin-bottom: 1.5rem;
 }
 
-.sold_out {
+.sold_out,
+.error {
   color: red;
+}
+
+input {
+  border: 0.025rem solid black;
 }
 </style>
 
@@ -37,7 +66,12 @@ export default {
   data: function() {
     return {
       message: "Welcome to My Site!",
-      showtimes: []
+      showtimes: [],
+      name: "",
+      email: "",
+      credit_card: "",
+      expiration_date: "",
+      errors: []
     };
   },
   created: function() {
@@ -49,10 +83,28 @@ export default {
     );
   },
   methods: {
+    // ticket create
     buyTicket: function(showtime) {
-      console.log(showtime);
+      this.errors = [];
+      var params = {
+        name: this.name,
+        email: this.email,
+        credit_card: this.credit_card,
+        expiration_date: this.expiration_date,
+        showtime_id: showtime.id
+      };
+      axios
+        .post("http://localhost:3000/api/tickets", params)
+        .then(function(response) {
+          console.log("RESPONSE: ", response);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
     }
   },
+  props: ["showtime"],
   computed: {}
 };
 </script>
